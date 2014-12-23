59b832b24897c3da0f774c627492d03a36f75e15
Masa <cmasad743@gmail.com>
2014-12-23 17:22:56 +0900

    fix

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,8 +1,7 @@
 $(function(){
   $('#clock').countdown('2015/01/01 00:00:00', function(event) {
   var $this = $(this).html(event.strftime(''
-  + '<span>%-w</span> week%!w '
-  + '<span>%-d</span> day%!d '
+  + '<span>%D</span> day%!d '
   + '<span>%H</span> hr '
   + '<span>%M</span> min '
   + '<span>%S</span> sec'));
