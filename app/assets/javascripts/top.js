e5f8fd28a81bd65220787a77ef382cebf60cf6ff
Michi Kanda <michi.kanda.0513@gmail.com>
2014-12-26 18:21:33 +0900

    Add top layout

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,10 +1,58 @@
-$(function(){
-  $('#clock').countdown('2015/01/01 00:00:00', function(event) {
-  var $this = $(this).html(event.strftime(''
-  + '<span>%-w</span> week%!w '
-  + '<span>%-d</span> day%!d '
-  + '<span>%H</span> hr '
-  + '<span>%M</span> min '
-  + '<span>%S</span> sec'));
- });
+ $(function(){
+   $('#clock').countdown('2015/01/01 00:00:00', function(event) {
+   var $this = $(this).html(event.strftime(''
+   + '<span>%-D</span> day%!D '
+   + '<span>%H</span> hour%!H '
+   + '<span>%M</span> minutes '
+   + '<span>%S</span> seconds'));
+    });
 });
+
+// $(function(){
+//   $('#clock').countDown({
+//         targetDate: {
+//             'day': 1,
+//             'month': 10,
+//             'year': 2015,
+//             'hour': 0,
+//             'min': 0,
+//             'sec': 0
+//         },
+//         omitWeeks: true
+//     });
+//   });
+
+var cbpAnimatedHeader = (function() {
+
+    var docElem = document.documentElement,
+        header = document.querySelector( '.navbar-inverse' ),
+        didScroll = false,
+        changeHeaderOn = 150;
+
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'navbar-scroll' );
+        }
+        else {
+            classie.remove( header, 'navbar-scroll' );
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
